53c961402e3ffb60d2d0750b21ac87581b29b2e7
Tony Oliver-Paull <toliverpaull@gmail.com>
2021-04-06 18:53:03 -0400

    ToolBar and ToolBarTitle components.

---
+++ src/components/ToolBar/ToolBarTitle.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './ToolBarTitle.module.css';
+
+const toolBarTitle = () => (
+  <img
+    src="/images/pool-pros-logo.png"
+    alt="Pool Pros"
+    title="POOL PROS"
+    className={styles.toolBarTitle}
+  />
+);
+
+export default toolBarTitle;
