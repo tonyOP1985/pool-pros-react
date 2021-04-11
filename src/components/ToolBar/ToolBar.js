84108454e454d0f76148051267f7f21ce65f8084
Tony Oliver-Paull <toliverpaull@gmail.com>
2021-04-11 03:32:11 -0400

    Nav Drawer Activator closes #17.

--- src/components/ToolBar/ToolBar.js
+++ src/components/ToolBar/ToolBar.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ToolBarTitle from './ToolBarTitle';
 import ToolBarMenu from './ToolBarMenu/ToolBarMenu';
 import ToolBarButton from '../Button/ToolBarButton';
+import NavDrawerActivator from './NavDrawerActivator';
 
 import styles from './ToolBar.module.css';
 
@@ -12,6 +13,7 @@ const toolBar = () => {
       <ToolBarTitle />
       <ToolBarMenu />
       <ToolBarButton />
+      <NavDrawerActivator />
     </nav>
   )
 };
