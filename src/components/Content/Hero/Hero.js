296e392e5456e6b373e17341be4c24a7bb24d8da
Tony Oliver-Paull <toliverpaull@gmail.com>
2021-04-11 22:24:11 -0400

    Hero image text (closes #6).

--- src/components/Content/Hero/Hero.js
+++ src/components/Content/Hero/Hero.js
@@ -4,21 +4,20 @@ import styles from './Hero.module.css';
 
 const hero = () => (
   <section className={styles.jumbotron} >
-    <div className={styles.jumbotronOverlay}></div>
-    {/* <div class="jumbotron-overlay">
-      <div class="jumbotron__message-main">
+    <div className={styles.jumbotronOverlay}>
+      <div className={styles.jumbotronMessageMain}>
         <h1>Is your pool ready for Summer?</h1>
       </div>
-      <div class="jumbotron__message-tag">
+      <div className={styles.jumbotronMessageTag}>
         <p>A pool pro is ready to help!</p>
       </div>
-      <div class="jumbotron__message-hr">
-        <hr>
+      <div className={styles.jumbotronMessageHR}>
+        <hr />
       </div>
-      <div class="jumbotron__message-sub">
+      <div className={styles.jumbotronMessageSub}>
         <p>Choose a pro that's close to your home from the list below.</p>
       </div>
-    </div> */}
+    </div>
   </section>
 );
 
